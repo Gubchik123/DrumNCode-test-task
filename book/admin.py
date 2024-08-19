5bdc0783b83fd1fa87ee339ab96bfe01fa6b7ee3
Nikita <nikita.gubarev31@gmail.com>
2024-08-19 12:01:27 +0300

    FEAT: Create BookAdmin

--- book/admin.py
+++ book/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Book
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    """Admin class for the Book model."""
+
+    list_display = (
+        "title",
+        "author",
+        "isbn",
+        "pages",
+        "language",
+        "published_date",
+    )
+    search_fields = ("title", "author", "isbn")
+    search_help_text = "Search by title, author, or ISBN."
+    list_filter = ("author", "published_date", "language")
