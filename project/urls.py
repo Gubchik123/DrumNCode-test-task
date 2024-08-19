010243a61ad292689aef74ce2161708e543fbf62
Nikita <nikita.gubarev31@gmail.com>
2024-08-19 12:17:16 +0300

    FEAT: Add API schema and Swagger endpoints to the root urlpatterns

--- project/urls.py
+++ project/urls.py
@@ -1,8 +1,17 @@
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import SpectacularSwaggerView, SpectacularAPIView
 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    # Local apps
     path("api/v1/", include("book.router")),
+    # Swagger
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "api/v1/schema/swagger/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
 ]
